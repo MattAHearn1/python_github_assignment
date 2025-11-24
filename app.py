1a8c79182258fc0c3d6d47432b516f5a1ac61821
Matthew A'Hearn <matthewahearn@usf.edu>
2025-11-23 21:12:36 -0500

    Error handling

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ hours = input("Enter hours worked:")
 hours = float(hours)
 daily_hours = hours / 7
 print("You studied " + daily_hours + "hours a day")
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+    
 
 
