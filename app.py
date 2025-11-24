af7873002062af65608c54fbaf13a628433f818f
Matthew A'Hearn <matthewahearn@usf.edu>
2025-11-23 21:16:51 -0500

    Clean up, comments, finalizing

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+#Program introduction
 print("Welcome to my python project!")
 hours = input("Enter hours worked:")
+
+#Completes calculations
 hours = float(hours)
 daily_hours = hours / 7
+
+#Final output
 print("You studied " + daily_hours + "hours a day")
+
+#Error handling
 try:
     hours = float(hours)
 except ValueError:
